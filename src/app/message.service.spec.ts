a6b7bcea8e2b59f5ffce3adb201d5b6f37c41bbb
marie.keane <marie.keane@centralia.edu>
2019-04-16 15:41:41 -0700

    message service tests

--- src/app/message.service.spec.ts
+++ src/app/message.service.spec.ts
@@ -9,4 +9,23 @@ describe('MessageService', () => {
     const service: MessageService = TestBed.get(MessageService);
     expect(service).toBeTruthy();
   });
+  
+  it('should add message', () => {
+    const service: MessageService = TestBed.get(MessageService);
+    expect(service.messages.length).toEqual(0);
+    service.add("Test");
+    expect(service.messages.length).toEqual(1);
+    expect(service.messages).toEqual(['Test']);
+  });
+  
+  it('should clear messages', () => {
+    const service: MessageService = TestBed.get(MessageService);
+    
+    service.add("Test1");
+    service.add("Test2");
+    service.add("Test3");
+    service.clear();
+    
+    expect(service.messages.length).toEqual(0);
+  });
 });
