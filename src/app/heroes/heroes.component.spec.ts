09ce7a01be8af8a835138522fdc85f169110a7bb
marie.keane <marie.keane@centralia.edu>
2019-04-22 10:31:59 -0700

    heroes component tests

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -1,8 +1,10 @@
+import { Component, DebugElement }   from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientModule } from '@angular/common/http';
 
 import { of } from 'rxjs'
+import { By } from '@angular/platform-browser';
 
 import { HeroesComponent } from './heroes.component';
 import { Hero } from '../hero';
@@ -13,18 +15,30 @@ describe('HeroesComponent', () => {
   let fixture: ComponentFixture<HeroesComponent>;
   
   let expectedHeroes: Hero[];
+  let heroToAdd: Hero;
+  let heroToDelete: Hero;
   
   let getHeroesSpy: jasmine.Spy;
+  let addHeroSpy: jasmine.Spy;
+  let deleteHeroSpy: jasmine.Spy;
 
   beforeEach(async(() => {
     expectedHeroes = [
         { id: 1, name: 'A' },
         { id: 2, name: 'B' },
+        { id: 3, name: 'C' },
+        { id: 4, name: 'D' },
+        { id: 5, name: 'E' },
        ] as Hero[];
+    
+    heroToAdd = { id: 6, name: 'F' } as Hero;
+    heroToDelete = expectedHeroes[2];
 
     // Create a fake HeroService object with a `getHeroes()` spy
-    const heroService = jasmine.createSpyObj('HeroService', ['getHeroes']);
+    const heroService = jasmine.createSpyObj('HeroService', ['getHeroes', 'addHero', 'deleteHero']);
     heroService.getHeroes.and.returnValue( of(expectedHeroes) );
+    heroService.addHero.and.returnValue( of(heroToAdd) );
+    heroService.deleteHero.and.returnValue( of(heroToDelete) );
     
     TestBed.configureTestingModule({
       imports: [
@@ -45,9 +59,79 @@ describe('HeroesComponent', () => {
     fixture.detectChanges();
     
     getHeroesSpy = fixture.debugElement.injector.get(HeroService).getHeroes as jasmine.Spy;
+    addHeroSpy = fixture.debugElement.injector.get(HeroService).addHero as jasmine.Spy;
+    deleteHeroSpy = fixture.debugElement.injector.get(HeroService).deleteHero as jasmine.Spy;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  
+  it('should display all heroes', () => {
+    expect(component.heroes).toEqual(expectedHeroes);
+    expect(getHeroesSpy.calls.any()).toBe(true, 'getHeroes called');
+  });
+
+  it('should add a hero when user clicks add', () => {
+    /* Arrange */
+    let newHeroes: Hero[] = expectedHeroes;
+    newHeroes.push(heroToAdd);
+    
+    const nameInputElement: HTMLInputElement = fixture.debugElement.query(By.css('#heroName')).nativeElement;
+    const addButton: HTMLButtonElement = fixture.debugElement.query(By.css('#addHero')).nativeElement;
+    
+    /* Act */
+    // Simulate user typing new hero name
+    nameInputElement.value = heroToAdd.name;
+    nameInputElement.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    // Simulate user clicking "Add" button
+    addButton.dispatchEvent(new Event('click'));
+    fixture.detectChanges();
+    
+    /* Assert */
+    // Check component property values
+    expect(component.heroes).toEqual(newHeroes);
+    expect(addHeroSpy.calls.any()).toBe(true, 'addHero called');
+    // Check template display
+    const heroListItems: DebugElement[] = fixture.debugElement.queryAll(By.css('.hero'));
+    expect(heroListItems.length).toEqual(newHeroes.length);
+  });
+  
+  it('should not add heroes without names', () => {
+    /* Arrange */
+    const addButton: HTMLButtonElement = fixture.debugElement.query(By.css('#addHero')).nativeElement;
+    
+    /* Act */
+    addButton.dispatchEvent(new Event('click'));
+    fixture.detectChanges();
+    
+    /* Assert */
+    expect(component.heroes).toEqual(expectedHeroes);
+    expect(addHeroSpy.calls.any()).toBe(false, 'addHero not called');
+    const heroListItems: DebugElement[] = fixture.debugElement.queryAll(By.css('.hero'));
+    expect(heroListItems.length).toEqual(expectedHeroes.length);
+  });
+  
+  it('should delete apppropriate hero when user clicks delete', () => {
+    /* Arrange */
+    let newHeroes: Hero[] = expectedHeroes;
+    newHeroes.splice(2, 1); // delete 3rd element
+    
+    /* Act */
+    // Simulate user deleting 3rd hero
+    const heroListItemElement: HTMLElement = fixture.debugElement.queryAll(By.css('.hero'))[2].nativeElement;
+    const deleteButton = heroListItemElement.lastElementChild;
+    deleteButton.dispatchEvent(new Event('click'));
+    fixture.detectChanges();
+    
+    /* Assert */
+    // Check component property values
+    expect(component.heroes).toEqual(newHeroes);
+    expect(deleteHeroSpy.calls.any()).toBe(true, 'getHeroes called');
+    // Check template display
+    const heroListItems: DebugElement[] = fixture.debugElement.queryAll(By.css('.hero'));
+    expect(heroListItems.length).toEqual(newHeroes.length);
+  });
+  
 });
