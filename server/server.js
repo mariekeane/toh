4df02af9b314a131233a7711cf1e3279276b91d9
marie.keane <marie.keane@centralia.edu>
2019-05-06 12:53:24 -0700

    refactor server files

--- server/server.js
+++ server/server.js
@@ -2,78 +2,36 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import Hero from './models/Hero';
+import * as mongoConfig from './config/config';
+import routes from './routes/index';
 
-const app = express();
-const router = express.Router();
 
-app.use(cors());
-app.use(bodyParser.json());
-
-mongoose.connect('mongodb://localhost:27017/toh', {useNewUrlParser: true, useFindAndModify: false});
-
+// Fixes for deprecation warnings
+/* 
+    Replace update() with updateOne(), updateMany(), or replaceOne()
+    Replace remove() with deleteOne() or deleteMany().
+    Replace count() with countDocuments(), unless you want to count how 
+        many documents are in the whole collection (no filter). In the latter 
+        case, use estimatedDocumentCount().
+ */
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+// Mongo Connection
+mongoose.connect(mongoConfig.mongoURL, {useNewUrlParser: true});
 const connection = mongoose.connection;
-
 connection.once('open', () => {
     console.log('MongoDB database connection established successfully!');
 });
 
-router.route('/heroes').get((req, res) => {
-    Hero.find((err, heroes) => {
-        if (err)
-            console.log(err);
-        else
-            res.json(heroes);
-    });
-});
+const app = express();
 
-router.route('/heroes/:id').get((req, res) => {
-    Hero.findById(req.params.id, (err, hero) => {
-        if (err)
-            console.log(err);
-        else
-            res.json(hero);
-    })
-});
+// Parser for POST data
+app.use(bodyParser.json());
+app.use(cors());
 
-router.route('/heroes').post((req, res) => {
-    let hero = new Hero(req.body);
-    hero.save()
-        .then(hero => {
-            res.status(200).json({'hero': 'Added successfully'});
-        })
-        .catch(err => {
-            res.status(400).send('Failed to create new record');
-        });
-});
+// Set up API routes
+app.use('/', routes);
 
-router.route('/heroes/:id').put((req, res) => {
-    Hero.findById(req.params.id, (err, hero) => {
-        if (!hero)
-            return next(new Error('Could not load Document'));
-        else {
-            hero.name = req.body.name;
-            hero.skills = req.body.skills;
-            hero.appointments = req.body.appointments;
-
-            hero.save().then(hero => {
-                res.json('Update done');
-            }).catch(err => {
-                res.status(400).send('Update failed');
-            });
-        }
-    });
-});
-
-router.route('/heroes/:id').delete((req, res) => {
-    Hero.findByIdAndRemove({_id: req.params.id}, (err, hero) => {
-        if (err)
-            res.json(err);
-        else
-            res.json('Removed successfully');
-    });
-});
-
-app.use('/', router);
-
+// Listen on provided port, on all network interfaces.
 app.listen(4000, () => console.log(`Express server running on port 4000`));
